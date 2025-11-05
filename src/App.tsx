e7676856a9470d73f1c206b880303cace76fb816
Ville Toikkanen <toikkanenville@gmail.com>
2025-11-05 08:32:44 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <div className={styles.container}>
+      <div className={styles.title}>
+        <b>SNAP</b>GRID
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
